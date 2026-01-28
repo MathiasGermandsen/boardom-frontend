9a4903ccb8bc63b4b95c076ef91855a837fd4b75
alex344b <alex344b@edu.mercantec.dk>
2026-01-28 13:29:45 +0100

    fixex and bug finding, should work

--- Boardom/Program.cs
+++ Boardom/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddzRazorComponents()
+builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 
